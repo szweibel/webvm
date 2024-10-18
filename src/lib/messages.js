8bea08c67182011b1e87a25185fef6d6a43fef6e
Stephen Zweibel <srzweibel@gmail.com>
2024-10-18 10:12:48 -0400

    Update messages.js

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -2,29 +2,6 @@ const color= "\x1b[1;35m";
 const underline= "\x1b[94;4m";
 const normal= "\x1b[0m";
 export const introMessage = [
-	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
-	"|                                                                             |",
-	"| WebVM is a virtual Linux environment running in the browser via WebAssembly |",
-	"|                                                                             |",
-	"| WebVM is powered by the CheerpX virtualization engine, which enables safe,  |",
-	"| sandboxed client-side execution of x86 binaries, fully client-side          |",
-	"|                                                                             |",
-	"| CheerpX includes an x86-to-WebAssembly JIT compiler, a virtual block-based  |",
-	"| file system, and a Linux syscall emulator                                   |",
-	"|                                                                             |",
-	"| Your own WebVM with custom images via Dockerfile:                           |",
-	"|                                                                             |",
-	"| " + underline + "https://leaningtech.com/mini-webvm-your-linux-box-from-dockerfile-via-wasm" + normal +"  |",
-	"|                                                                             |",
-	"+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+",
-	"",
-	"   Welcome to WebVM. If unsure, try these examples:",
-	"",
-	"     python3 examples/python3/fibonacci.py ",
-	"     gcc -o helloworld examples/c/helloworld.c && ./helloworld",
-	"     objdump -d ./helloworld | less -M",
-	"     vim examples/c/helloworld.c",
-	"     curl --max-time 15 parrot.live  # requires networking",
 	""
 ];
 export const errorMessage = [
