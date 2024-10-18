c60eab639bca27d88df36fe63122aae9d5863544
Stephen Zweibel <srzweibel@gmail.com>
2024-10-18 10:25:02 -0400

    Update WebVM.svelte

--- src/lib/WebVM.svelte
+++ src/lib/WebVM.svelte
@@ -295,9 +295,8 @@
 </script>
 
 <main class="relative w-full h-full">
-	<Nav />
 	<div class="absolute top-10 bottom-0 left-0 right-0">
-		<SideBar on:connect={handleConnect} on:reset={handleReset}/>
+		
 		{#if configObj.needsDisplay}
 			<canvas class="absolute top-0 bottom-0 left-14 right-0" width="1024" height="768" id="display"></canvas>
 		{/if}
